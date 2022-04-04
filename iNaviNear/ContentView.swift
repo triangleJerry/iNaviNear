3e57a7d1e2871ace77e76e47368120f9e8633912
eunseock <dortus47@naver.com>
2022-04-04 11:51:01 +0900

    - SwiftUI기반 프로젝트 생성, Deployment Target 14.0 변경

---
+++ iNaviNear/ContentView.swift
@@ -0,0 +1,22 @@
+
+//
+//  ContentView.swift
+//  iNaviNear
+//
+//  Created by 장은석 on 2022/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iNaviNear/iNaviNearApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNaviNearApp.swift
+//  iNaviNear
+//
+//  Created by 장은석 on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct iNaviNearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
