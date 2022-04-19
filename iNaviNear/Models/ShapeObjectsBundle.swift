0d606a94e712654a6422b20b881dc0f05d74f740
dortus47 <dortus47>
2022-04-19 18:44:33 +0900

    - 탐색알고리즘 원형 -> 최단직선거리로 변경

--- iNaviNear/Models/ShapeObjectsBundle.swift
+++ iNaviNear/Models/ShapeObjectsBundle.swift
@@ -18,6 +18,8 @@ class ShapeObjectsBundle {
     var CafeDataArray: [CafeData] = []
     var circleArray: [INVCircle] = []
     var markerArray: [INVMarker] = []
+    var distanceDictionary: [INVMarker:TrkData] = [:]
+    var lineArray: [INVPolyline] = []
 
     private init() {
         
@@ -32,10 +34,10 @@ class ShapeObjectsBundle {
     private func getINVcircleArray() -> [INVCircle] {
         
         var INVcircleArray: [INVCircle] = []
-        for (index, item) in trkArray.enumerated() {
+        for (index, trk) in trkArray.enumerated() {
             if index % 100 == 0 {
                 let circle = INVCircle()
-                circle.center = item.location
+                circle.center = trk.location
                 circle.radius = Double(UserDefaults.standard.integer(forKey: "area"))
                 circle.fillColor = UIColor( cgColor: UIColor(red: 0.90, green: 0.67, blue: 0.64, alpha: 0.30).cgColor )
                 circle.strokeWidth = 5 // 원의 테두리 두께를 5pt로 설정
@@ -47,6 +49,43 @@ class ShapeObjectsBundle {
         
         return INVcircleArray
     }
+    
+    
+    // 최단거리 표시를 위한 INVPolyline 객체들을 일괄적으로 만들어 받아오는 메소드.
+    private func getLineDictionary() -> [INVMarker:TrkData] {
+        
+        var _distanceDictionary: [INVMarker:TrkData]  = [:]
+        for (_, marker) in markerArray.enumerated() {
+            let result = SearchingArea.searchShortestDistance(trkArray: trkArray, marker: marker)
+            guard result != nil else {
+                continue
+            }
+            _distanceDictionary[marker] = result
+        }
+
+        return _distanceDictionary
+    }
+    
+    // 주행경로 <-> 마커 사이의 최단거리 확보를 위한 쌍을 일괄적으로 만들어 오는 메소드.
+    private func getINVPolylineArray() -> [INVPolyline] {
+        
+        var _polylineArray: [INVPolyline] = []
+        var num = 0
+        for trkAndMarker in distanceDictionary {
+            let coords = [
+                INVLatLng(lat: trkAndMarker.key.position.lat, lng: trkAndMarker.key.position.lng),
+                INVLatLng(lat: trkAndMarker.value.location.lat, lng: trkAndMarker.value.location.lng)
+            ]
+            let polyline = INVPolyline()
+            polyline.coords = coords
+            polyline.width = 5
+            polyline.color = .red
+            _polylineArray.append(polyline)
+        }
+        
+        return _polylineArray
+    }
+    
     
     // 아이나비 지도 위에 마커(카페)들을 그려주기 위해 INVMarker가 담긴 배열을 반환해 주는 메소드.
     private func getINVMarkerArray(mapView: InaviMapView) -> [INVMarker] {
@@ -60,12 +99,8 @@ class ShapeObjectsBundle {
             marker.iconScale = 0.5
             marker.iconScale += (item.element.star.CGFloatValue()! / 10.0 )
             marker.title = item.element.name
-            marker.iconImage = SearchingArea.searchInAreaMarker(circleArray: circleArray, marker: marker)
             
-            if UserDefaults.standard.bool(forKey: "markers") == false
-                && marker.iconImage == INV_MARKER_IMAGE_GRAY {
-                continue
-            }
+            marker.iconImage = INV_MARKER_IMAGE_GRAY
             
             marker.touchEvent = { (shape) in
                 HapticManager.instance.impact(style: .medium)
@@ -99,24 +134,38 @@ class ShapeObjectsBundle {
         return INVMarkerArray
     }
     
+    // 범위 내에 포함되는 마커들의 아이콘 색상을 변경시켜주는 메소드
+    private func changeINVMarkerIconImage() {
+        
+        for marker in markerArray.enumerated() {
+            if distanceDictionary[marker.element] != nil {
+                marker.element.iconImage = INV_MARKER_IMAGE_BLUE
+            }
+        }
+    }
+    
     // 아이나비 지도 위에 셰이프 되어 있는 객체들을 지워주는 메소드. ( 경로 제외 )
     func removeAllMapShapeObjects() {
         
-        ShapeUtils.removeCircleRanges(circleArray: circleArray)
         ShapeUtils.removeINVMarkers(markerArray: markerArray)
+        ShapeUtils.removeINVPolylines(lineArray: lineArray)
         
-        circleArray = []
         markerArray = []
+        lineArray = []
+        distanceDictionary = [:]
     }
     
     // 아이나비 지도 위에 경로, 탐색범위, 마커를 일괄적으로 셰이프 해주는 메소드.
     func drawMapShapeObjects(mapView: InaviMapView) {
         
-        circleArray = getINVcircleArray()
         markerArray = getINVMarkerArray(mapView: mapView)
+        distanceDictionary = getLineDictionary()
+        lineArray = getINVPolylineArray()
+        
+        changeINVMarkerIconImage() // 최단거리 계산 이후, 포함되는 마커들의 아이콘 이미지를 변경
         
         ShapeUtils.shapeINVRoutes(trkDataArray: trkArray, mapView: mapView)
-        ShapeUtils.shapeCircleRanges(mapView: mapView, circleArray: circleArray)
+        ShapeUtils.shapeLines(mapView: mapView, linesArray: lineArray)
         ShapeUtils.shapeINVMarkers(mapView: mapView, markerArray: markerArray)
     }
 }
--- iNaviNear/Utils/ShapeUtils.swift
+++ iNaviNear/Utils/ShapeUtils.swift
@@ -33,10 +33,19 @@ class ShapeUtils {
     }
     
     // 아이나비 지도 위에 원형 범위를 셰이프 해주는 메소드.
-    static func shapeCircleRanges( mapView: InaviMapView, circleArray: [INVCircle]) {
+    static func shapeCircleRanges(mapView: InaviMapView, circleArray: [INVCircle]) {
         
         for circle in circleArray.enumerated() {
             circle.element.mapView = mapView
+        }
+    }
+    
+    // 아이나비 지도 위에 최단 거리( 직선 )를 셰이프 해주는 메소드.
+    static func shapeLines(mapView: InaviMapView, linesArray: [INVPolyline]) {
+        
+        for line in linesArray.enumerated() {
+            
+            line.element.mapView = mapView
         }
     }
     
@@ -44,11 +53,32 @@ class ShapeUtils {
     static func shapeINVMarkers(mapView: InaviMapView, markerArray: [INVMarker]) {
         
         for marker in markerArray.enumerated() {
-
+            if UserDefaults.standard.bool(forKey: "markers") == false
+                && marker.element.iconImage == INV_MARKER_IMAGE_GRAY {
+                continue
+            }
             marker.element.mapView = mapView
         }
     }
     
+    // 아이나비 지도 위에 최단거리들을 셰이프 해주는 메소드.
+    static func shapeINVPolylines(mapView: InaviMapView, lineArray: [INVPolyline]) {
+        
+        for line in lineArray.enumerated() {
+
+            line.element.mapView = mapView
+        }
+    }
+    
+    // 범위 내에 포함되는 마커들의 아이콘 색상을 변경시켜주는 메소드
+    static func changeINVMarkerIconImage(markerArray: [INVMarker], distanceDictionary: [INVMarker:TrkData]) {
+        
+        for marker in markerArray.enumerated() {
+            if distanceDictionary[marker.element] != nil {
+                marker.element.iconImage = INV_MARKER_IMAGE_BLUE
+            }
+        }
+    }
     
     // MARK: iNaviMaps remove Objects func.
     
@@ -63,6 +93,13 @@ class ShapeUtils {
     static func removeINVMarkers(markerArray: [INVMarker]) {
         for marker in markerArray.enumerated() {
             marker.element.mapView = nil
+        }
+    }
+    
+    // 아이나비 지도 위의 최단거리 선들을 일괄적으로 지워주는 메소드.
+    static func removeINVPolylines(lineArray: [INVPolyline]) {
+        for line in lineArray.enumerated() {
+            line.element.mapView = nil
         }
     }
 }
